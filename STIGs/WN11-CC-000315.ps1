75b0c2dfc16f484ee20398a70ed2700341a87d93
William Olega <126032694+williamolega@users.noreply.github.com>
2025-11-11 16:16:06 -0500

    Create WN11-CC-000315.ps1

---
+++ STIGs/WN11-CC-000315.ps1
@@ -0,0 +1,26 @@
+<#
+.SYNOPSIS
+    
+
+.NOTES
+    Author          : William Olega
+    LinkedIn        : linkedin.com/in/williamolega/
+    GitHub          : github.com/williamolega
+    Date Created    : 2025-11-11
+    Last Modified   : 2025-11-11
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : 
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\.ps1 
+#>
